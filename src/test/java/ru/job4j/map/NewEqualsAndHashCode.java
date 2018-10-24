aa04abe1a083cf31404f35b43ad4b63be397f2f3
Ruslan_Lagoiko <Ruslan_Lagoiko@epam.com>
2018-10-24 17:42:23 +0300

    Created NeqEqualsAndHashCode.java

---
+++ src/test/java/ru/job4j/map/NewEqualsAndHashCode.java
@@ -0,0 +1,46 @@
+package ru.job4j.map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class NewEqualsAndHashCode {
+
+    @Test
+    public void name() {
+        Map<User, Object> map = new HashMap<>();
+        Calendar calendar = Calendar.getInstance();
+        NewUser a = new NewUser("A", 1, calendar);
+        NewUser b = new NewUser("A", 1, calendar);
+        map.put(a, "1");
+        map.put(b, "1");
+        System.out.println(map);
+        Assert.assertEquals(a, b);
+        Assert.assertEquals(a.hashCode(), b.hashCode());
+    }
+
+    private class NewUser extends User {
+        NewUser(String name, int children, Calendar birthday) {
+            super(name, children, birthday);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return super.getChildren() == user.getChildren() &&
+                    Objects.equals(super.getName(), user.getName()) &&
+                    Objects.equals(super.getBirthday(), user.getBirthday());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(super.getName(), super.getChildren(), super.getBirthday());
+        }
+    }
+}
