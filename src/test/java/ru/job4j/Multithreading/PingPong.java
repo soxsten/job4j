7cda638b9c8e54527ede88f248258cd5c6d61ad2
Ruslan_Lagoiko <Ruslan_Lagoiko@epam.com>
2018-11-01 14:47:24 +0300

    Created PingPong.java

---
+++ src/test/java/ru/job4j/Multithreading/PingPong.java
@@ -0,0 +1,69 @@
+package ru.job4j.Multithreading;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class PingPong extends Application {
+    private static final String JOB4J = "Пинг-понг www.job4j.ru";
+
+    @Override
+    public void start(Stage stage) {
+        int limitX = 300;
+        int limitY = 300;
+        Group group = new Group();
+        Rectangle rect = new Rectangle(50, 100, 10, 10);
+        group.getChildren().add(rect);
+        new Thread(new RectangleMove(rect)).start();
+        stage.setScene(new Scene(group, limitX, limitY));
+        stage.setTitle(JOB4J);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
+
+class RectangleMove implements Runnable {
+    private final Rectangle rect;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        double speed = 5;
+        double speedX = speed;
+        double speedY = speed;
+        while (true) {
+            double currentX = this.rect.getX();
+            double newPosX = currentX + speed;
+            if (shouldChangeDirectionFor(newPosX)) {
+                speedX = changeDirectionFor(speedX);
+            }
+            this.rect.setX(currentX + speedX);
+
+            double currentY = this.rect.getY();
+            double newPosY = currentY + speed;
+            if (shouldChangeDirectionFor(newPosY)) {
+                speedY = changeDirectionFor(speedY);
+            }
+            this.rect.setY(currentY + speedY);
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean shouldChangeDirectionFor(double pos) {
+        return pos >= 300 || pos <= 0;
+    }
+
+    private double changeDirectionFor(double dir) {
+        return dir * -1;
+    }
+}
