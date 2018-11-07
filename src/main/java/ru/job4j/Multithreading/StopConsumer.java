d74701a145fbeb12a9d41b23965eeb31a210cbe2
Ruslan_Lagoiko <Ruslan_Lagoiko@epam.com>
2018-11-07 17:57:00 +0300

    Done

--- src/main/java/ru/job4j/Multithreading/StopConsumer.java
+++ src/main/java/ru/job4j/Multithreading/StopConsumer.java
@@ -1,38 +1,29 @@
 package ru.job4j.Multithreading;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class StopConsumer {
 
     public static void main(String[] args) throws InterruptedException {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
-        AtomicInteger offerTimes = new AtomicInteger(0);
         Thread producer = new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
                         queue.offer(index);
-                        offerTimes.getAndIncrement();
                         sleepFor(500);
                     }
                 }
         );
-        AtomicInteger pollTimes = new AtomicInteger(0);
+        producer.start();
+        producer.join();
         final Thread consumer = new Thread(
                 () -> {
-                    while (true) {
-                        if (producer.getState().equals(Thread.State.TERMINATED)) {
-                            if (pollTimes.get() == offerTimes.get()) {
-                                break;
-                            }
-                        }
+                    while (Thread.currentThread().isInterrupted()) {
                         System.out.println(queue.poll());
-                        pollTimes.getAndIncrement();
-                        sleepFor(1000);
+                        sleepFor(500);
                     }
                 }
         );
-        producer.start();
         consumer.start();
+        consumer.interrupt();
     }
 
     private static void sleepFor(int time) {
